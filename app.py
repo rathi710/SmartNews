c07a13191ecee877c136e1618b4da711e6c33284
rathi310 <vishesh.rathi.710@gmail.com>
2023-04-06 23:02:11 +0530

    final code

---
+++ app.py
@@ -0,0 +1,37 @@
+#Implement all this concept by machine learning with flask
+
+from flask import Flask, request, render_template
+import pickle
+
+vector = pickle.load(open("vectorizer.pkl", 'rb'))
+model = pickle.load(open("finalized_model.pkl", 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/newsfeed')
+def newsfeed():
+    return render_template("newsfeed.html")
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    if request.method == "POST":
+        news = str(request.form['news'])
+        print(news)
+
+        predict = model.predict(vector.transform([news]))[0]
+        print(predict)
+
+        return render_template("prediction.html", prediction_text="The news is predicted as -> {}".format(predict))
+
+
+    else:
+        return render_template("prediction.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
